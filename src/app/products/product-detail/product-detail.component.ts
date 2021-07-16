19aa1a29a4ae498658ac61be217a291327ebce5b
Tatiroa994 <tatico994@gmail.com>
2021-07-15 22:41:53 -0500

    ajuste product-detail

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,32 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { IProduct } from 'src/app/shared/models/product.model';
+import { ProductService } from 'src/app/shared/services/product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css'],
 })
-export class ProductDetailComponent implements OnInit {
-  pageTitle: string = 'product detail';
+export class ProductDetailComponent implements OnInit, OnDestroy {
+  pageTitle = 'Product Detail';
+  errorMessage = '';
   product: IProduct | undefined;
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  sub!: Subscription;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private productService: ProductService
+  ) {}
 
   ngOnInit(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.pageTitle += `  ${id}`;
-    this.product = {
-      productId: 5,
-      productName: 'Hammer',
-      productCode: 'TBX-0048',
-      releaseDate: 'May 21, 2021',
-      description: 'Curved claw steel hammer',
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: 'assets/images/hammer.png',
-    };
+    if (id) {
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number): void {
+    this.sub = this.productService.getProduct(id).subscribe({
+      next: (product) => (this.product = product),
+      error: (err) => (this.errorMessage = err),
+    });
   }
 
   onBack(): void {
     this.router.navigate(['/products']);
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
   }
 }
